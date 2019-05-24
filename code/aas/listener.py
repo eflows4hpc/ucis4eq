b28bf7be7dd37080910d46b0961b835b963e3ec5
Juan Esteban Rodríguez <juane2rodriguez>
2019-05-24 14:36:16 +0200

    Rewriten code to be object oriented.
    AAS:
        - The listener was rewriten for support object and webservices
        specializations.
        - New example added.
        - Fixed bug while retrieving RAW data from webservices.

--- code/aas/listener.py
+++ code/aas/listener.py
@@ -4,7 +4,7 @@
 # This module is part of the Automatic Alert System (AAS) solution
 # 
 # Author:  Juan Esteban Rodríguez, Josep de la Puente
-# Contact: juan.rodriguez@bsc.es, josep.delapuente@bsc.es
+# Contact: juan.rodriguez@bself.es, josep.delapuente@bself.es
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,6 +22,8 @@
 ################################################################################
 # Module imports
 import os
+import uuid 
+import io
 import sys
 import traceback
 import argparse
@@ -29,10 +31,192 @@ import json
 import requests
 import sched, time
 import multiprocessing
+import obspy
+import datetime
 
 ################################################################################
 # Methods and classes
 
+# Interface class 
+class WSGeneral:
+
+    
+    # Methos
+    def __init__(self, config):
+        # Object UUID
+        #self.uuid = uuid.uuid1()
+        
+        # Store the configuration parameters 
+        self.config = config
+        
+        # Attributes
+        self.results = {}
+        
+        # Create the recursive scheduler
+        self.s = sched.scheduler(time.time, time.sleep)
+        
+        # Config such scheduler
+        self.s.enter(0, 1, self.exploreRepositories, ())
+        
+        # Start the AAS service
+        self.s.run()
+    
+    # Explore the set of input repositories    
+    def exploreRepositories(self, pool = None):
+        # Prepare the pool of processes
+        np = len(self.config['repositories'])
+        
+        ws = self.config['webservice']
+        output = {}
+        
+        #print("Creating pool with", str(np), "processes")
+        
+        # Create the processes pool just one
+        if not pool:
+            pool = multiprocessing.Pool(np)
+        
+        query = ws['interface'] + "/" + ws['majorversion'] + "/"
+        query = query + ws["application"] + "?"
+        for p in ws["parameters"]:
+            query = query + p + "=" + ws["parameters"][p]+"&"
+        
+        query = query.strip('&')
+        
+        # Create the file name 
+            
+        # For each provided repository
+        tasks = [(self.requestRepository, 
+                 (r['name'],
+                  r['url']+query)) for r in self.config['repositories']]
+        imap_unordered_it = pool.imap_unordered(self.runtask, tasks)
+        
+        # Wait the tasks to finish
+        for name, result in imap_unordered_it:
+            self.results[name] = result
+                
+        # Store the current timestamp
+        self.postprocess_actions()
+        
+        # Generate JSON file containing the generated metada
+        for r in self.config['repositories']:
+            repo = {}
+            repo['url'] = r['url']+query
+            repo['data'] = r['name']+self.config['listener']['data_ext']
+            output[r['name']] = repo
+
+        with open(self.config['listener']['results_name'], "w") as f:
+            json.dump(output, f, indent=4)
+        
+        # Start running the triggering system
+        #os.system(self.config['listener']['trigger'] + "&")
+        
+        # Queue the following job
+        interval = self.config['listener']['interval']
+        if( interval > 0 ):
+            #print("Next report in ", interval, "seconds")
+            self.s.enter(interval, 1, self.exploreRepositories, kwargs={'pool': pool})
+        
+    # Do the REST-GET petition
+    def requestRepository(self, name, url):
+        # Request data
+        #print("Requesting info from '" + url + "'")
+        self.r = requests.get(url, stream=True)
+                
+        # Process data
+        return (name, self.process_data(self.r, name))
+            
+    # Process obtained data
+    def process_data(self, results, name):
+        # Write file to disk
+        with open(name+self.config['listener']['data_ext'], 'wb') as f:
+            f.write(results.content)
+            
+    # Post-process actions
+    def postprocess_actions(self):
+        pass
+            
+    # For parallel computing    
+    def __execute(self,func, args):
+        result = func(*args)
+        return result
+
+    def runtask(self, args):
+        return self.__execute(*args)
+        
+class WSEvents(WSGeneral):
+    
+    def __init__(self, config):
+        # Attributes
+        self.currenttime = None
+        
+        #WSGeneral.__init__(self,config)
+        super(WSEvents, self).__init__(config)
+        #self.uuid = uuid.uuid1()
+    
+    def process_data(self, results, name):
+        # Variables 
+        params = self.config['webservice']['parameters']
+        
+        # Write the original file to disk
+        super(WSEvents, self).process_data(results, name)
+            
+        # Check the requested format 
+        if( 'format' in params.keys() and params['format'] == "xml"):
+            return self.process_xml_data(results, name)
+        
+        return 0
+            
+    # Process a QuakeML data
+    def process_xml_data(self, results, name):
+        # Variables
+        now = None
+        delay = None
+        cat = None
+        
+        # Set the time threshold 
+        if( name in self.results.keys() ):
+            currenttime = self.results[name]
+        else:
+            currenttime = 0.0
+        
+        try:
+            # Read the set of events
+            cat = obspy.read_events(name+self.config['listener']['data_ext'], "QUAKEML")
+        except Exception as error:
+            #print("Obspy error", error )
+            return 0
+        
+        #print(name, ":", self.a)
+        #self.a = cat.count()
+        #print("[", name, "] --> ", cat.count(),"events found")
+            
+        for e in cat:
+            if( currenttime < e.origins[0]['time'].timestamp ):
+                now = datetime.datetime.now(datetime.timezone.utc)
+                delay = now.timestamp() - e.origins[0]['time'].timestamp
+                print(now,
+                    "[", name, "] --> ",  e.origins[0]['time'].datetime, 
+                    e.origins[0]['latitude'], e.origins[0]['longitude'], 
+                    e.origins[0]['depth'], e.magnitudes[0]['mag'],
+                    "Delay (secs):", delay)
+                # TODO Do something here!
+                    
+        #print(name, ":", self.a)
+        #print( cat.__str__(print_all=True) )
+        #print("[", name, "] --> ",  cat[0].origins[0]['time'], 
+        #    cat[0].origins[0]['latitude'], cat[0].origins[0]['longitude'], 
+        #    cat[0].origins[0]['depth'], cat[0].magnitudes[0]['mag'])
+            #self.currenttime = cat[1]['creation_info']['creation_time']
+        return cat[0].origins[0]['time'].timestamp
+    
+    # Post-process actions
+    def postprocess_actions(self):
+        #self.currenttime = time.time()
+        pass
+
+def format_ts(ts):
+    return datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+                
 # Arguments parser
 def parser():
     
@@ -49,93 +233,21 @@ def parser():
     
     # Return them
     return args
-    
-def calculate(func, args):
-    result = func(*args)
-    return '%s says that %s%s = %s' % (
-        multiprocessing.current_process().name,
-        func.__name__, args, result
-        )
-
-def calculatestar(args):
-    return calculate(*args)    
-    
-def requestRepository(name, url):
-    # Request data
-    print("Requesting info from '" + url + "'")
-    r = requests.get(url)
-
-    # Write file to disk
-    f = open(name, 'w')
-    f.write(r.text)
-    f.close()
-    
-def exploreRepositories(sc, pool = None):
-    
-    # Prepare the pool of processes
-    np = len(sc.config['repositories'])
-    
-    ws = sc.config['webservice']
-    output = {}
-    
-    #print("Creating pool with", str(np), "processes")
-    
-    # Create the processes pool just one
-    if not pool:
-        pool = multiprocessing.Pool(np)
-    
-    query = ws['interface'] + "/" + ws['majorversion'] + "/"
-    query = query + ws["application"] + "?"
-    for p in ws["parameters"]:
-        query = query + p + "=" + ws["parameters"][p]+"&"
-    
-    query = query.strip('&')
-    
-    # Create the file name 
-    
-    # For each provided repository
-    tasks = [(requestRepository, 
-             (r['name']+sc.config['listener']['data_ext'],
-              r['url']+query)) for r in sc.config['repositories']]
-    imap_unordered_it = pool.imap_unordered(calculatestar, tasks)
-    
-    # Generate JSON file containing the generated metada
-    for r in sc.config['repositories']:
-        repo = {}
-        repo['url'] = r['url']+query
-        repo['data'] = r['name']+sc.config['listener']['data_ext']
-        output[r['name']] = repo
-
-    
-    with open(sc.config['listener']['results_name'], "w") as f:
-        json.dump(output, f, indent=4)
-    
-    # Start running the triggering system
-    os.system(sc.config['listener']['trigger'] + "&")
-    
-    # Queue the following job
-    sc.enter(sc.config['listener']['interval'], 1, exploreRepositories, (sc, pool))
 
 # Main Method
 def main():
-    try:
+    try:    
         # Call the parser
         args = parser()
-        
-        # Create the recursive scheduler
-        s = sched.scheduler(time.time, time.sleep)
-        
+                
         # Read the configuration file
         with open(args.config, 'r') as f:
-            s.config = json.load(f)
-                
-        
-        # Config such scheduler
-        
-        s.enter(0, 1, exploreRepositories, (s,))
-
-        # Start the AAS service
-        s.run()
+            config = json.load(f)
+            
+        if( config['webservice']['interface'] == "event" ):
+            ws = WSEvents(config)
+        else:
+            ws = WSGeneral(config)
         
     except Exception as error:
         print("Exception in code:")
