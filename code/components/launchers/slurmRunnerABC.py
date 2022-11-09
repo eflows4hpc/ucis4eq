0142397ab2824f93e8499606b1abb4bf09531731
martapien <marta.pienkowska@erdw.ethz.ch>
2022-11-09 16:29:10 +0100

    add skipping job submission of the SUCCESS file is alrady present; so if you re-run the workflow on the same folder with the same folder structure

--- code/components/launchers/slurmRunnerABC.py
+++ code/components/launchers/slurmRunnerABC.py
@@ -77,37 +77,53 @@ class SlurmRunnerABC(RunnerABC, ABC):
         
         tcmd = "cd " + path + "; sbatch " + cmd
 
-        # Run the command and wait
-        command = [self.baseCmd, self.proxyFlag, self.proxyCmd, remote, tcmd]
+        # MPC Don't resubmit the job if the job already successfully finished in the given repo.\
+        # ToDo this is not the neatest way to do it, as all input files will be overwritten in the process. The check
+        # ToDo should happen before the input files get pushed?
+        command_find = [self.baseCmd, self.proxyFlag, self.proxyCmd, remote, "find", path + "/SUCCESS"]
 
-        # Check for and error
-        command_trials = 0
-        while True:
-            process = subprocess.run(list(filter(None, command)),
-                                     stdout=subprocess.PIPE,
-                                     stdin=subprocess.PIPE,
-                                     stderr=subprocess.PIPE,
-                                     encoding='utf8')
+        process_find = subprocess.run(list(filter(None, command_find)),
+                                      stdout=subprocess.PIPE,
+                                      stdin=subprocess.PIPE,
+                                      stderr=subprocess.PIPE,
+                                      encoding='utf8')
+        if "SUCCESS" in process_find.stdout:
+            # Results are already available in the given directory, so we can skip.
+            print("INFO: the job in %s has previously completed successfully and is not submitted again. "
+                  "If you wish for the workflow to re-run this, you need to remove the existing directories or define "
+                  "a new directory name for the execution." % path)
+            self.resultAvailable.set()
+        else:
+            # Run the command and wait
+            command = [self.baseCmd, self.proxyFlag, self.proxyCmd, remote, tcmd]
 
-            if process.returncode != 0:
-                if command_trials < 3:
-                    print("\nWARNING: Command '" + self.baseCmd + " " + remote + " " + tcmd
-                          + "' failed. Error: " + process.stderr + "Retrying in 15 seconds.", flush=True)
-                    time.sleep(15)
-                    command_trials += 1
-                    continue
+            # Check for and error
+            command_trials = 0
+            while True:
+                process = subprocess.run(list(filter(None, command)),
+                                         stdout=subprocess.PIPE,
+                                         stdin=subprocess.PIPE,
+                                         stderr=subprocess.PIPE,
+                                         encoding='utf8')
+
+                if process.returncode != 0:
+                    if command_trials < 3:
+                        print("\nWARNING: Command '" + self.baseCmd + " " + remote + " " + tcmd
+                              + "' failed. Error: " + process.stderr + "Retrying in 15 seconds.", flush=True)
+                        time.sleep(15)
+                        command_trials += 1
+                        continue
+                    else:
+                        raise Exception("Command '" + self.baseCmd + " " + remote + " "
+                                       + tcmd + "' failed three times in a row. Error: " + process.stderr)
                 else:
-                    raise Exception("Command '" + self.baseCmd + " " + remote + " "
-                                   + tcmd + "' failed three times in a row. Error: " + process.stderr)
-            else:
-                break
+                    break
 
+            # Obtain the job ID
+            jobId = [x.strip() for x in process.stdout.split(' ')][3]
 
-        # Obtain the job ID
-        jobId = [x.strip() for x in process.stdout.split(' ')][3]
-        
-        thread = threading.Thread(target=self._waitForSlurmJob, args=(jobId,path))
-        thread.start()
+            thread = threading.Thread(target=self._waitForSlurmJob, args=(jobId,path))
+            thread.start()
 
         # Wait for the process to finish
         timeout = 5
@@ -137,11 +153,11 @@ class SlurmRunnerABC(RunnerABC, ABC):
                                       encoding='utf8')
                                       
             # Check for a connection error:
-            # if the connection fails, wait for 15 s and try to reconnect
+            # if the connection fails, wait for 60 s and try to reconnect
             if process.returncode != 0:
-                time.sleep(60)
                 print("\nWARNING: Command that checks the job status '" + self.baseCmd + " " + remote + " " + cmd
                       + "' failed. Error: " + process.stderr + "Retrying in 60 seconds.", flush=True)
+                time.sleep(60)
                 continue
                 # raise Exception("Command '" + self.baseCmd + " " + remote + " "
                 #                 + cmd + "' failed. Error: " + process.stderr)
@@ -150,17 +166,33 @@ class SlurmRunnerABC(RunnerABC, ABC):
             self.message = process.stdout.rstrip()
 
             # Stop condition:
-            # if the job is not in the queue, stop checking and assume that the job is finished
-            # MPC ToDo here if the job is not in the queue it is assumed to be finished successfully,
-            # but it is not actually always true. Need a different mechanism of checking and/or retrying.
+            # if the job is not in the queue AND the SUCCESS file exists in the run directory,
+            # stop checking and assume that the job is finished
             if process.stdout == "":
                 command = [self.baseCmd, self.proxyFlag, self.proxyCmd, remote, "find", rundir + "/SUCCESS"]
 
-                process = subprocess.run(list(filter(None, command)),
-                                         stdout=subprocess.PIPE,
-                                         stdin=subprocess.PIPE,
-                                         stderr=subprocess.PIPE,
-                                         encoding='utf8')
+                # Check for and error
+                command_trials = 0
+                while True:
+                    process = subprocess.run(list(filter(None, command)),
+                                             stdout=subprocess.PIPE,
+                                             stdin=subprocess.PIPE,
+                                             stderr=subprocess.PIPE,
+                                             encoding='utf8')
+
+                    if process.returncode != 0:
+                        if command_trials < 3:
+                            print("\nWARNING: Command '" + self.baseCmd + " " + remote + " find " + rundir + "/SUCCESS"
+                                  + "' failed. Error: " + process.stderr + "Retrying in 15 seconds.", flush=True)
+                            time.sleep(15)
+                            command_trials += 1
+                            continue
+                        else:
+                            raise Exception("Command '" + self.baseCmd + " " + remote + " find " + rundir + "/SUCCESS"
+                                            + "' failed three times in a row. Error: " + process.stderr)
+                    else:
+                        break
+
                 if "SUCCESS" in process.stdout:
                     # Results are available
                     print("INFO: the job submitted in %s completed successfully." %rundir)
@@ -168,30 +200,84 @@ class SlurmRunnerABC(RunnerABC, ABC):
                     break
                 else:
                     # Results are not available, the job is not in the queue but seems like it did not complete
-                    # successfully either.
+                    # successfully either, as the SUCCESS file does not exist.
 
-                    # MPC Dump info from .e and .o to the log?
+                    # MPC Dump info from .e and .o to the log
                     command_e = [self.baseCmd, self.proxyFlag, self.proxyCmd, remote, "cat", rundir + "/*.e"]
+
+                    # Check for and error
+                    command_trials = 0
+                    while True:
+                        process_e = subprocess.run(list(filter(None, command_e)),
+                                                   stdout=subprocess.PIPE,
+                                                   stdin=subprocess.PIPE,
+                                                   stderr=subprocess.PIPE,
+                                                   encoding='utf8')
+
+                        if process_e.returncode != 0:
+                            if command_trials < 3:
+                                print(
+                                    "\nWARNING: Command '" + self.baseCmd + " " + remote + " cat " + rundir + "/*.e"
+                                    + "' failed. Error: " + process.stderr + "Retrying in 15 seconds.", flush=True)
+                                time.sleep(15)
+                                command_trials += 1
+                                continue
+                            else:
+                                raise Exception(
+                                    "Command '" + self.baseCmd + " " + remote + " cat " + rundir + "/*.e"
+                                    + "' failed three times in a row. Error: " + process.stderr)
+                        else:
+                            break
+
                     command_o = [self.baseCmd, self.proxyFlag, self.proxyCmd, remote, "cat", rundir + "/*.o"]
-                    process_e = subprocess.run(list(filter(None, command_e)),
-                                             stdout=subprocess.PIPE,
-                                             stdin=subprocess.PIPE,
-                                             stderr=subprocess.PIPE,
-                                             encoding='utf8')
-                    process_o = subprocess.run(list(filter(None, command_o)),
-                                             stdout=subprocess.PIPE,
-                                             stdin=subprocess.PIPE,
-                                             stderr=subprocess.PIPE,
-                                             encoding='utf8')
 
-                    print("\nWARNING: something went wrong in %s." %rundir)
-                    print("\nWARNING: stderr: \n" + process_e.stdout)
-                    print("\nWARNING: stdout: \n" + process_o.stdout)
+                    # Check for and error
+                    command_trials = 0
+                    while True:
+                        process_o = subprocess.run(list(filter(None, command_o)),
+                                                   stdout=subprocess.PIPE,
+                                                   stdin=subprocess.PIPE,
+                                                   stderr=subprocess.PIPE,
+                                                   encoding='utf8')
 
-                    self.resultAvailable.clear()
-                    raise Exception(
-                        "The job submitted in %s seems to not have completed successfully. Automatic "
-                        "resubmission is not implemented yet, please check the error manually." %rundir)
+                        if process_o.returncode != 0:
+                            if command_trials < 3:
+                                print(
+                                    "\nWARNING: Command '" + self.baseCmd + " " + remote + " cat " + rundir + "/*.o"
+                                    + "' failed. Error: " + process.stderr + "Retrying in 15 seconds.", flush=True)
+                                time.sleep(15)
+                                command_trials += 1
+                                continue
+                            else:
+                                raise Exception(
+                                    "Command '" + self.baseCmd + " " + remote + " cat " + rundir + "/*.o"
+                                    + "' failed three times in a row. Error: " + process.stderr)
+                        else:
+                            break
+
+                    # MPC ToDo this is Salvus specific (perhaps even on daint only?),
+                    # but I don't know how to solve this otherwise.
+                    if "Floating point exception (core dumped)" in process_e.stdout:
+                        # MPC ToDo finish this: ideally we'd like to resubmit here automatically...
+                        print("\nWARNING: something went wrong with the Salvus simulation in %s. "
+                              "This should be resubmitted and will run." % rundir)
+                        # # ToDo I need to add here the buffer that the ssh command doesn't fail.
+                        # # MPC clean up: remove all core.* files from the directory.
+                        # command_r = [self.baseCmd, self.proxyFlag, self.proxyCmd, remote, "rm", rundir + "/core.*"]
+                        # process_r = subprocess.run(list(filter(None, command_r)),
+                        #                            stdout=subprocess.PIPE,
+                        #                            stdin=subprocess.PIPE,
+                        #                            stderr=subprocess.PIPE,
+                        #                            encoding='utf8')
+                    else:
+                        print("\nWARNING: something went wrong in %s." %rundir)
+                        print("\nWARNING: stderr: \n" + process_e.stdout)
+                        print("\nWARNING: stdout: \n" + process_o.stdout)
+
+                        self.resultAvailable.clear()
+                        raise Exception(
+                            "The job submitted in %s seems to not have completed successfully. Automatic "
+                            "resubmission is not implemented yet, please check the error manually." %rundir)
 
             else:
                 time.sleep(60)
