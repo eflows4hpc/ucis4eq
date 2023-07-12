2a29cac310a4c142451ac1418747ca8ad14edd9b
Cbhihe <cedric.bhihe@gmail.com>
2023-07-12 18:47:57 +0200

    Fixed paths in docker-compose.yml and bug in exit code test in install.sh

--- deployment/README.md
+++ deployment/README.md
@@ -10,7 +10,7 @@ _________________
   ```
   Find more installation options in:
   https://docs.docker.com/install/linux/docker-ce/ubuntu/
-  
+
 * Docker compose
   ```
   sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/bash
 
 # UCIS4EQ Instalation
-printf "%s\n" "Welcome to UCIS4EQ instalation"
+printf "\n%s\n" "==============================="
+printf "%s\n" "Welcome to UCIS4EQ installation."
+printf "%s\n\n" "==============================="
 
 # Creating the dir structure
-printf "%s\n" "Dreating the directories structure ..."
+printf "%s\n" "Creating the directories structure ..."
 mkdir -p deployment/dockers/ data/
 
 # Downloading files from repository
-printf "%s\n" "Downloading files from the https://gitlab.bsc.es/wavephenomenagroup/ucis4eq/ repository"
-printf "%s\n" "........ for service deployment..."
+printf "%s\n" "Downloading files from the https://gitlab.bsc.es/wavephenomenagroup/ucis4eq/ repository."
 printf "%s\n" "(Already existing files in the destination directory will be overwritten.)"
+printf "%s\n" "... for service deployment..."
 
 # The PRIVATE-TOKEN provided below gives full programmatic READ access to the code
 # repository and to the Docker image registry. The user's scope is assimilated to
@@ -18,24 +20,27 @@ printf "%s\n" "(Already existing files in the destination directory will be over
 
 curl --silent GET --header 'PRIVATE-TOKEN: ER9nSBQo8xsiczs47pAn' 'https://gitlab.bsc.es/api/v4/projects/2703/repository/files/docker-compose.yml/raw?ref=migration-fixes' >| docker-compose.yml
 
-if ! "$?"; then
-    printf "\n\n%s\n" "Installation process aborted. Please check the \`install.sh' script present in your"
+if [ $? -ne 0 ]; then
+    printf "\n%s\n" "---------------------------------------"
+    printf "%s\n" "Installation process aborted. Please check the \`install.sh' script present in your"
     printf "%s\n" "installation directory and make sure:"
     printf "%s\n" " - either that your GitLab instance's \`PRIVATE-TOKEN', i.e. the GitLab Project Access Token"
     printf "%s\n" " (PAT), is still valid to access resources on https://gitlab.bsc.es/wavephenomenagroup/ucis4eq/"
     printf "%s\n\n" " - or that the fetched file resources are available in that same repo."
+    printf "%s\n\n" "---------------------------------------"
     exit 1
 fi
 
 curl --silent GET --header 'PRIVATE-TOKEN: ER9nSBQo8xsiczs47pAn' 'https://gitlab.bsc.es/api/v4/projects/2703/repository/files/docker-compose-add-ssh-key-to-docker.yml/raw?ref=migration-fixes' >| docker-compose-add-ssh-key-to-docker.yml
 curl --silent GET --header 'PRIVATE-TOKEN: ER9nSBQo8xsiczs47pAn' 'https://gitlab.bsc.es/api/v4/projects/2703/repository/files/deployment%2Fdockers%2FDockerfile-credentials/raw?ref=migration-fixes' >| deployment/dockers/Dockerfile-credentials
 
-printf "%s\n" "........ for compute and data repositories setup..."
+printf "%s\n" "... for compute and data repositories setup..."
 curl --silent GET --header 'PRIVATE-TOKEN: ER9nSBQo8xsiczs47pAn' 'https://gitlab.bsc.es/api/v4/projects/2703/repository/files/data%2FDAL.template.json/raw?ref=migration-fixes' >| data/DAL.json
 curl --silent GET --header 'PRIVATE-TOKEN: ER9nSBQo8xsiczs47pAn' 'https://gitlab.bsc.es/api/v4/projects/2703/repository/files/data%2FSites.template.json/raw?ref=migration-fixes' >| data/Sites.json
 curl --silent GET --header 'PRIVATE-TOKEN: ER9nSBQo8xsiczs47pAn' 'https://gitlab.bsc.es/api/v4/projects/2703/repository/files/data%2Fresources.xml/raw?ref=migration-fixes' >| data/resources.xml
 curl --silent GET --header 'PRIVATE-TOKEN: ER9nSBQo8xsiczs47pAn' 'https://gitlab.bsc.es/api/v4/projects/2703/repository/files/data%2Fproject.xml/raw?ref=migration-fixes' >| data/project.xml
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 echo ""
 echo "Now:"
@@ -50,21 +55,27 @@ echo " 6- To make an installation clean-up"
 echo "    docker-compose down"
 =======
 printf "%s\n" "........ and a test event..."
+=======
+printf "%s\n" "... and a test event..."
+>>>>>>> e25f4bd (Fixed paths in docker-compose.yml and bug in exit code test in install.sh)
 curl --silent GET --header 'PRIVATE-TOKEN: ER9nSBQo8xsiczs47pAn' 'https://gitlab.bsc.es/api/v4/projects/2703/repository/files/data%2FMED_SAMOS_IZMIR_IRIS_test.json/raw?ref=migration-fixes' >| data/MED_SAMOS_IZMIR_IRIS_test.json
 
-printf "%s\n\n" "All DONE!"
-printf "%s\n" "Now:"
+printf "\n%s\n" "Done. Now ..."
 printf "%s\n" " 1- Revise the files data \`Sites.json' and \`DAL.json' to set your own credentials as well"
 printf "%s\n" "    paths specific to your installation."
 printf "%s\n" " 2- Make sure that \`docker' and \`docker-compose' are installed locally"
 printf "%s\n"  "   ---> 'https://docs.docker.com/compose/install/'"
 printf "%s\n" " 3- Login to the Gitlab UCIS4EQ registry with:"
-printf "%\t\ts\n" "> docker login registry.gitlab.bsc.es"
+printf "%s\n" "    > docker login registry.gitlab.bsc.es"
 printf "%s\n" " 4- To start runnning the services, issue either:"
-printf "%s\t\t\n" "> docker compose up"
-printf "%s\n" "or:"
-printf "%s\t\t\n" "> SSH_PRV=$(cat ~/.ssh/your_id_rsa_file)  docker-compose up"
+printf "%s\n" "    > docker compose up"
+printf "%s\n" "    or:"
+printf "%s\n" '    > SSH_PRV=$(cat ~/.ssh/your_id_rsa_file)  docker-compose up'
 printf "%s\n" " 5- To stop running the services, press: Ctrl+C  (twice)"
 printf "%s\n" " 6- To clean-up after running Docker containers:"
+<<<<<<< HEAD
 printf "%s\t\t\n" "> docker-compose down"
 >>>>>>> 45f9372 (updated README.md files and instructions for UCIS4EQ deployment)
+=======
+printf "%s\n" "    > docker-compose down"
+>>>>>>> e25f4bd (Fixed paths in docker-compose.yml and bug in exit code test in install.sh)
